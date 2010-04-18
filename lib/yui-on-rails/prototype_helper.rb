caea970538a040cf2eb0215a0fc15fa77cd9d311
jeff durand <jeff.durand@gmail.com>
2010-04-18 01:49:12 -0400

    overridding default rails helpers.

--- lib/yui-on-rails/prototype_helper.rb
+++ lib/yui-on-rails/prototype_helper.rb
@@ -1,11 +1,13 @@
-module YuiOnRails
-  module PrototypeHelper
+module ActionView
+  module Helpers
+    module PrototypeHelper
     #alias :original_link_to_remote :link_to_remote
     
-    def link_to_remote_with_yui(name, options = {}, html_options = nil)
-      options.merge!(:before=>"alert('blah blah')")
-      link_to_remote_without_yui(name, options, html_options)
+      def link_to_remote_with_yui(name, options = {}, html_options = nil)
+        options.merge!(:before=>"alert('blah blah')")
+        link_to_remote_without_yui(name, options, html_options)
+      end
+      alias_method_chain :link_to_remote, :yui
     end
-    alias_method_chain :link_to_remote, :yui
   end
 end
