9d0829de45d90a20c27727da08e3f33651ba9fdc
jeff durand <jeff.durand@gmail.com>
2010-04-17 23:43:06 -0400

    changing to class based

--- lib/yui-on-rails/tabs.rb
+++ lib/yui-on-rails/tabs.rb
@@ -1,37 +1,83 @@
 # FlickadayYui
 module YuiOnRails
   module Tabs
-      @@tab_content = []
-      @@tab_titles = []
+    def tabs_for(*options, &block)
+      tabs = YuiOnRails::Tabs::TabsRenderer.new(*options, &block)
+      tabs_html = tabs.render
+      return concat(tabs_html)
+    end
     
-      def yui_tabs(id,&block)
+    class TabsRenderer
+    
+      def initialize( options={}, &block )
         raise ArgumentError, "Missing block" unless block_given?
-        capture(&block)
-        concat(content_tag(:div, :class=>"yui-navset tabber", :id=>id) do
-          content_tag(:ul, yui_get_links, :class=>"yui-nav")+content_tag(:div,yui_get_content,:class=>"yui-content")
-        end,block.binding)  
-        @@tab_content = []
-        @@tab_titles = []
+        @template = eval( 'self', block.binding )
+        @options = options
+        @tabs = []
+        yield self
       end
-      def yui_get_links
-        links = ""
-        @@tab_titles.each_with_index do |title,index|
-          links += content_tag(:li,link_to(content_tag(:em, title), "#tab#{index}"),:class=>"#{index==0 ? "selected" : ""}")
+      
+      def create(tab_id,tab_text,options={},&block)
+        raise "Block needed for TabsRenderer#CREATE" unless block_given?
+        @tabs << [ tab_id, tab_text, options, block ]
+      end
+      
+      def render
+        content_tag(:div,(render_tabs+render_bodies),{:id=>:tabs}.merge(@options))
+      end
+      
+      private # ---------------------------------------------------------------------------
+      def render_tabs
+        content_tag :ul do
+          @tabs.collect do |tab|
+            content_tag( :li, link_to( content_tag( :span, tab[1] ), "##{tab[0]}" ) )
+          end.join
         end
-        return links
       end
-      def yui_get_content
-        content = ""
-        @@tab_content.each_with_index do |cont, index|
-          content += content_tag(:div, cont.to_s, :id=>"tab#{index}")
-        end
-        return content
+
+      def render_bodies
+        @tabs.collect do |tab|
+          content_tag( :div, capture( &tab[3] ), tab[2].merge( :id => tab[0] ) )
+        end.join.to_s
       end
-      def yui_tab(title, &block)
-        raise ArgumentError, "Missing block" if title.blank?
-        raise ArgumentError, "Missing block" unless block_given?
-        @@tab_content << capture(&block)
-        @@tab_titles << title
+
+      def method_missing( *args, &block )
+        @template.send( *args, &block )
       end
+    end
+    
+    
+      #@@tab_content = []
+      #@@tab_titles = []
+    
+      #def yui_tabs(id,&block)
+      #  raise ArgumentError, "Missing block" unless block_given?
+      #  capture(&block)
+      #  concat(content_tag(:div, :class=>"yui-navset tabber", :id=>id) do
+      #    content_tag(:ul, yui_get_links, :class=>"yui-nav")+content_tag(:div,yui_get_content,:class=>"yui-content")
+      #  end,block.binding)  
+      #  @@tab_content = []
+      #  @@tab_titles = []
+      #end
+      #def yui_get_links
+      #  links = ""
+      #  @@tab_titles.each_with_index do |title,index|
+      #    links += content_tag(:li,link_to(content_tag(:em, title), "#tab#{index}"),:class=>"#{index==0 ? "selected" : ""}")
+      #  end
+      #  return links
+      #end
+      #def yui_get_content
+      #  content = ""
+      #  @@tab_content.each_with_index do |cont, index|
+      #    content += content_tag(:div, cont.to_s, :id=>"tab#{index}")
+      #  end
+      #  return content
+      #end
+      #def yui_tab(title, &block)
+      #  raise ArgumentError, "Missing block" if title.blank?
+      #  raise ArgumentError, "Missing block" unless block_given?
+      #  @@tab_content << capture(&block)
+      #  @@tab_titles << title
+      #end
   end
 end
