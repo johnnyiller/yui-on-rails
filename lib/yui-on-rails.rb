7a2443772490f4f7c5b331237351e8e363b5f14c
jeff durand <jeff.durand@gmail.com>
2010-04-28 15:11:15 -0400

    adding support for radio buttons

--- lib/yui-on-rails.rb
+++ lib/yui-on-rails.rb
@@ -1,2 +1,3 @@
 require File.join(File.dirname(__FILE__), *%w[yui-on-rails tabs])
 require File.join(File.dirname(__FILE__), *%w[yui-on-rails prototype_helper])
+require File.join(File.dirname(__FILE__), *%w[yui-on-rails radio_buttons])
---
+++ lib/yui-on-rails/radio_buttons.rb
@@ -0,0 +1,42 @@
+module YuiOnRails
+  module RadioButtons
+    def radio_buttons_for(*options, &block)
+      radio_buttons = YuiOnRails::RadioButtons::RadioButtonsRenderer.new(*options, &block)
+      radios_html = radio_buttons.render
+      return concat(radios_html)
+    end
+    
+    class RadioButtonsRenderer
+    
+      def initialize( options={}, &block )
+        raise ArgumentError, "Missing block" unless block_given?
+        @template = eval( 'self', block.binding )
+        @options = options
+        @radio_buttons = []
+        yield self
+      end
+      
+      def create(button_name,button_text,options={})
+        raise "You must provide a button name dummy.#CREATE" if button_name.blank?
+        @radio_buttons << [button_name,button_text, options]
+      end
+      
+      def render
+        content_tag(:div,render_bodies,{:id=>"button_group", :class=>"yui-buttongroup"}.merge(@options))
+      end
+      
+      private # ---------------------------------------------------------------------------
+      def render_bodies
+        #content_tag :div, :class=>"yui-content" do
+          @radio_buttons.collect do |radio_button|
+            radio_button_tag(radio_buttons[0], radio_buttons[1], false, radio_buttons[2])
+          end.join.to_s
+        #end
+      end
+      def method_missing( *args, &block )
+        @template.send( *args, &block )
+      end
+      
+    end
+  end
+end
