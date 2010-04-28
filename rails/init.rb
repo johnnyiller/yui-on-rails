c24018af61b55282c822a4d73f1161b46d764855
jeff durand <jeff.durand@gmail.com>
2010-04-28 15:36:11 -0400

    adding support for radio buttons

--- rails/init.rb
+++ rails/init.rb
@@ -1,3 +1,4 @@
 require File.join(File.dirname(__FILE__), *%w[.. lib yui-on-rails])
 ActionView::Base.send :include, YuiOnRails::Tabs
+ActionView::Base.send :include, YuiOnRails::RadioButtons
 #ActionView::Helpers::PrototypeHelper.send :include, YuiOnRails::PrototypeHelper
